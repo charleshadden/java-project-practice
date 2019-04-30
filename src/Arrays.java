4820201f83365b2a243c8b0ea214a6705b051ca0
Hadden <charles.hadden@accenturefederal.com>
2019-04-30 16:01:06 -0500

    'added-arrays'

---
+++ src/Arrays.java
@@ -0,0 +1,12 @@
+public class Arrays {
+
+    static String[] pets = {"Diesel", "Fraulein", "Cat"};
+    public static void main(String[] args) {
+        for ( String pet : pets) {
+            System.out.println(pet);
+        }
+
+    }
+
+
+}
